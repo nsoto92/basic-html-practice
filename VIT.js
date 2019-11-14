85c987b2e8777a08b7e15ac0f5645bb5b70fcdd7
nsoto92 <you@example.com>
2019-11-13 20:26:05 -0800

    VetsInTech

---
+++ VIT.js
@@ -0,0 +1,4 @@
+var link = document.getElementById('resume');
+link.addEventListener("click", function() {
+    alert("Congratulations! You are one step closer to hiring your next employee");
+  });
